73aa1cb9f1f2f6b78d41cdfb24b7c4c80996dd43
carlosazevedo2006 <carlosalbertotininazevedo2006@gmail.com>
2025-12-04 02:47:42 +0000

    correcao de bugs

--- JogoCobra/src/components/GameBoard.tsx
+++ JogoCobra/src/components/GameBoard.tsx
@@ -4,116 +4,64 @@ import { View, StyleSheet, Animated } from "react-native";
 import { CELULA } from "../utils/constants";
 
 export default function GameBoard({
+  gridSize,
   cobra,
-  cobraInimiga,
-  enemyAnimSegments,   // <-- ADICIONADO
+  cobraAnim,
   comida,
-  animSegments,
-  eatAnim,
-  corCobra,
-  modoSelecionado,
+  comidaAnim,
   panHandlers,
 }: any) {
-  return (
-    <View style={styles.wrapper} {...panHandlers}>
-      {/* Pixel Border */}
-      <View pointerEvents="none" style={styles.crtOverlay}/>
-      <View style={styles.pixelBorder} />
+  const boardSize = gridSize * CELULA;
 
-      {/* Main Grid */}
-      <View style={styles.board}>
-        {/* --- Comida (pixel) --- */}
+  return (
+    <View
+      style={[styles.wrapper, { width: boardSize, height: boardSize }]}
+      {...panHandlers}
+    >
+      {/* FOOD */}
+      <Animated.View
+        style={[
+          styles.food,
+          {
+            transform: [
+              { translateX: comida.x * CELULA },
+              { translateY: comida.y * CELULA },
+              { scale: comidaAnim },
+            ],
+          },
+        ]}
+      />
+
+      {/* SNAKE SEGMENTS */}
+      {cobraAnim.map((anim: any, i: number) => (
         <Animated.View
+          key={i}
           style={[
-            styles.food,
+            styles.snake,
             {
               transform: [
-                { translateX: comida.x * CELULA },
-                { translateY: comida.y * CELULA },
-                { scale: eatAnim },
+                { translateX: anim.x },
+                { translateY: anim.y },
               ],
             },
           ]}
         />
-
-        {/* --- Cobra (pixel) --- */}
-        {cobra.map((seg: any, i: number) => {
-          const anim = animSegments[i];
-          return (
-            <Animated.View
-              key={i}
-              style={[
-                styles.snake,
-                { backgroundColor: corCobra },
-                anim && {
-                  transform: anim.getTranslateTransform(),
-                },
-              ]}
-            />
-          );
-        })}
-
-        {/* --- Cobra Inimiga (pixel) (AGORA SUAVE) --- */}
-        {modoSelecionado === "DIFICIL" && enemyAnimSegments?.current?.[0] && (
-          <Animated.View
-            style={[
-              styles.enemy,
-              {
-                transform: [
-                  { translateX: enemyAnimSegments.current[0].x },   // <-- ADICIONADO
-                  { translateY: enemyAnimSegments.current[0].y },   // <-- ADICIONADO
-                ],
-              },
-            ]}
-          />
-        )}
-
-      </View>
+      ))}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    width: CELULA * 10,
-    height: CELULA * 10,
-    position: "relative",
-  },
-
-  scorePixel: {
-    color: "#00ff66",
-    fontSize: 28,
-    fontFamily: "VT323_400Regular",
-    letterSpacing: 2,
-    marginBottom: 10,
-  },
-
-  pixelBorder: {
-    position: "absolute",
-    top: -6,
-    left: -6,
-    right: -6,
-    bottom: -6,
-    borderWidth: 6,
-    borderColor: "#00ff66",
-  },
-
-  board: {
-    width: "100%",
-    height: "100%",
     backgroundColor: "#000",
+    position: "relative",
+    overflow: "hidden",
   },
 
   snake: {
     width: CELULA,
     height: CELULA,
     backgroundColor: "#43a047",
-  },
-
-  enemy: {
-    width: CELULA,
-    height: CELULA,
-    backgroundColor: "#ff3333",
     position: "absolute",
   },
 
